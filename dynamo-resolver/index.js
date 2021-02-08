7cc5ca7e2ed7f92020923b32d3efca14d535b031
deepak <deepaksantosh.r@gmail.com>
2021-02-08 01:17:27 -0500

    added products service

--- dynamo-resolver/index.js
+++ dynamo-resolver/index.js
@@ -1,5 +1,33 @@
+const AWS = require('aws-sdk');
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
+
 module.exports = {
-    helloWorld() {
-      return "Hello world" + " from dependency";
+  async getRecord(table, id){
+    console.log('inside dynamo-resolver');
+    const params = {
+      TableName: table,
+      Key: {
+        id
+      }
     }
-  };
+    try {
+      const data = await dynamoDBClient.get(params).promise();
+      return {
+        statusCode: 200,
+        body: data.Item
+      };
+    } catch (error) {
+      console.error('Error in the getRecord call of dynamo resolver', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(
+          {
+            message: 'Error in the resolver'
+          },
+          null,
+          2
+        )
+      };
+    }
+  }
+}
--- product-service/handler.js
+++ product-service/handler.js
@@ -1,7 +1,7 @@
 const dynamoResolver = require("dynamo-resolver");
 module.exports.hello = async event => {
-  console.log("Received call in Users");
-  const record = await dynamoResolver.getRecord('users', '1');
+  console.log("Received call in Products");
+  const record = await dynamoResolver.getRecord('products', '3');
   return {
     statusCode: 200,
     body: JSON.stringify(
